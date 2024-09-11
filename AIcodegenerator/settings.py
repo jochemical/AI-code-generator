3cd0a067507d55c2802a2fe2af144d5e6094ffa3
jochemical <jgkemp002@gmail.com>
2024-09-11 18:15:41 +0200

    additions

--- AIcodegenerator/settings.py
+++ AIcodegenerator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-x(a=2x1j!^e=kv6ku25yta5$agfsadtakq#v&9_m-j7hqg*u*u"
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
